ab4c95ecffe3b2f9588f21367dc52a57bd083dd0
turbopasi <office@pascal-lamers.at>
2019-11-02 19:48:17 +0100

    meta.js fix for template

--- meta.js
+++ meta.js
@@ -4,22 +4,7 @@
 module.exports = {
 
   prompts: {
-    name: {
-      type: 'string',
-      required: true,
-      message: 'Enter Project Name',
-      default: 'vuesr-clone'
-    },
-    description: {
-      type: 'string',
-      required: false,
-      message: 'Enter Description',
-      default: 'A Vuesr Template Project'
-    },
-    author: {
-      type: 'string',
-      message: 'Author'
-    }
+
   },
 
   completeMessage: 'Setup Complete'
