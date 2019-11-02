d4c2fdc950cccebb4efdee9225a8448f4a108727
turbopasi <office@pascal-lamers.at>
2019-11-02 12:07:41 +0100

    Fixed 1px Y shift on hover

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,6 @@
 
 <style lang="scss">
 
-  // * {
-  //   margin : 0;
-  //   padding : 0;
-  //   box-sizing: border-box;
-  //   font-family : sans-serif;
-  // }
-
   body {
     background : #F8F8F8;
   }
