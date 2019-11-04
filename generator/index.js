40f139a3ee4983943f7049ddad02891986c86560
turbopasi <office@pascal-lamers.at>
2019-11-04 22:55:33 +0100

    Added vuex and vue-router to plugin

--- generator/index.js
+++ generator/index.js
@@ -3,10 +3,12 @@ module.exports = api => {
   api.extendPackage({
     dependencies: {
       'buefy': '^0.8.6',
-      'vue-perfect-scrollbar': '^0.2.0'
+      'vue-perfect-scrollbar': '^0.2.0',
+      'vue-router': '^3.1.3',
+      'vuex': '^3.1.1'
     }
   })
 
   api.render('./template')
-  
+
 }
